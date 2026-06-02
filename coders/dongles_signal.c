3fd9d77fbd6432e08c4ac2cd5aa1578ba12c310b
raulpedreno <raulpedrenoperez@gmail.com>
2026-06-02 16:42:41 +0200

    norminette pass

--- coders/dongles_signal.c
+++ coders/dongles_signal.c
@@ -1,21 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   dongles_signal.c                                          :+:      :+:    :+:   */
+/*   dongles_signal.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rpedreno <rpedreno@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/05/28 15:32:09 by rpedreno          #+#    #+#             */
-/*   Updated: 2026/05/28 15:32:12 by rpedreno         ###   ########.fr       */
+/*   Created: 2026/06/02 16:38:46 by rpedreno          #+#    #+#             */
+/*   Updated: 2026/06/02 16:40:48 by rpedreno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../codexion.h"
 
-
 void	wake_all_dongles(t_sim *sim)
 {
-	int	i;
+	int		i;
 
 	i = 0;
 	while (i < sim->config.number_of_coders)
